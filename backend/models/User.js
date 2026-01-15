937ddb68215b9b20ec2ccc49babf757c81ec46f5
SahilKumar-projects <sahilkumarr1704@gmail.com>
2026-01-15 21:30:07 +0530

    fix ui

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,18 +5,20 @@ const UserSchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
-      trim
+      trim: true,
     },
+
     email: {
       type: String,
       required: true,
       unique: true,
       lowercase: true,
+      trim: true,
     },
+
     password: {
       type: String,
       required: true,
-      minlength: 6,
     },
   },
   { timestamps: true }
